eadf7f0a095fa09e46dc0467752fbb8d8ed060e4
Tejal Kumbhar <tejalkumbhar5258@gmail.com>
2025-02-10 15:55:18 +0530

    update exist supplier

--- quantbit_steel_subcontracting/quantbit_steel_subcontracting/doctype/subcontracting_out/subcontracting_out.py
+++ quantbit_steel_subcontracting/quantbit_steel_subcontracting/doctype/subcontracting_out/subcontracting_out.py
@@ -56,7 +56,7 @@ class SubcontractingOut(Document):
 		self.update_on_purchase_order()
 
 	def make_job_work_in(self):
-		if frappe.db.exists("Supplier", self.supplier_id,'is_internal_supplier'):
+		if frappe.get_value("Supplier", self.supplier_id,'is_internal_supplier'):
 			if self.company in frappe.get_all("Allowed To Transact With", filters={'parent': self.supplier_id}, pluck='company'):
 				comp = frappe.get_value("Supplier", self.supplier_id, 'represents_company')
 				customer = frappe.get_value("Customer",filters={'represents_company': self.company},fieldname='name')
@@ -78,7 +78,7 @@ class SubcontractingOut(Document):
 					if not jwi_warehouse:
 						frappe.throw(f"Set Job Work In Target Warehouse At Job Work Settings For {comp}")
 					if po.subcontracting_operation:
-						job_work_operation = frappe.db.exists("Job Work Operations",po.subcontracting_operation)
+						job_work_operation = frappe.get_value("Job Work Operations",po.subcontracting_operation)
 						if not job_work_operation:
 							frappe.throw(f"Create {po.subcontracting_operation} Operation in Job Work Opertation")
 						
